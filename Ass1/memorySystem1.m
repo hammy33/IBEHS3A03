739f838e99b4114e4ddc83726286b8915d9fbebe
Hamrish Saravanakumar <saravah@mcmaster.ca>
2021-10-05 13:18:41 -0400

    initial commits for testing memory

---
+++ Ass1/memorySystem1.m
@@ -0,0 +1,19 @@
+% Matlab script to test the memory property for System 1.
+% Running this script should output a subplot that is further analyzed in the report section of the assignment.
+
+n = -5:5;
+a = [0 0 0 0 0 1 0 0 0 0 0];
+b = [0 0 0 0 1 1 0 0 0 0 0];
+ 
+original = system1(n, a);
+shifted = system1(n, b);
+ 
+subplot(2,2,1);
+originalinput = stem(n, a), xlabel('n'), ylabel('x[n]'),title('Original Input');
+subplot(2,2,2);
+shiftedinput = stem(n, b), xlabel('n'), ylabel('x[n]'),title('Shifted Input');
+subplot(2,2,3);
+originaloutput = stem(n, original),xlabel('n'),ylabel('y[n]'),title('Original Output');
+subplot(2,2,4);
+shiftedoutput = stem(n, shifted),xlabel('n'),ylabel('y[n]'),title('Shifted Output');
+sgtitle('System 1 - Memory Test');
---
+++ Ass1/memorySystem2.m
@@ -0,0 +1,19 @@
+% Matlab script to test the memory property for System 2.
+% Running this script should output a subplot that is further analyzed in the report section of the assignment.
+
+n = -5:5;
+a = [0 0 0 0 0 1 0 0 0 0 0];
+b = [0 0 0 0 1 1 0 0 0 0 0];
+ 
+original = system2(n, a);
+shifted = system2(n, b);
+ 
+subplot(2,2,1);
+originalinput = stem(n, a), xlabel('n'), ylabel('x[n]'),title('Original Input');
+subplot(2,2,2);
+shiftedinput = stem(n, b), xlabel('n'), ylabel('x[n]'),title('Shifted Input');
+subplot(2,2,3);
+originaloutput = stem(n, original),xlabel('n'),ylabel('y[n]'),title('Original Output');
+subplot(2,2,4);
+shiftedoutput = stem(n, shifted),xlabel('n'),ylabel('y[n]'),title('Shifted Output');
+sgtitle('System 2 - Memory Test');
