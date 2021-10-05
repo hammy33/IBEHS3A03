2ad625040811944c3a7ebaf73482af647998e33c
Hamrish Saravanakumar <saravah@mcmaster.ca>
2021-10-05 13:18:20 -0400

    initial commits for testing causality

---
+++ Ass1/causalitySystem2.m
@@ -0,0 +1,13 @@
+% Matlab script to test the causality property for System 2.
+% Running this script should output a subplot that is further analyzed in the report section of the assignment.
+
+n = -5:5;
+x = [0 0 0 0 0 0 1 2 3 4 5];
+ 
+system = system2(n, x);
+ 
+subplot(2,1,1);
+input = stem(n, x), xlabel('n'), ylabel('x[n]'),title('Input');
+subplot(2,1,2);
+output = stem(n, system),xlabel('n'),ylabel('y[n]'),title('Output');
+sgtitle('System 2 - Causality Test');
