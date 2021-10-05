297d9ea761f289df0cd3b2a6a6c8e5c730735d5c
Hamrish Saravanakumar <saravah@mcmaster.ca>
2021-10-05 12:04:24 -0400

    initial commits for testing time invariance

---
+++ Ass1/timeInvarianceSystem2.m
@@ -0,0 +1,21 @@
+% Matlab script to test the time invariant property for System 2. 
+% Running this script should output a subplot that is further analyzed in the report section of the assignment.
+
+n = -5:5;
+x = [0 0 0 0 0 1 0 0 0 0 0];
+y = [0 1 0 0 0 0 0 0 0 0 0];
+
+a = system2(n, x);
+b = system2(n, y);
+
+subplot(2,2,1);
+input1 = stem(n,x),xlabel('n'),ylabel('x[n]'),title('Original Input');
+subplot(2,2,2);
+input2 = stem(n,y),xlabel('n'),ylabel('x[n]'),title('New Input');
+subplot(2,2,3);
+output1 = stem(n,a),xlabel('n'),ylabel('y[n]'),title('Original Output');
+subplot(2,2,4);
+output2 = stem(n,b),xlabel('n'),ylabel('y[n]'),title('New Output');
+sgtitle('System 2 - Time Invariance Test') 
+
+% It was determined that system 2 is time invariant.
