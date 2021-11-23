a1df2f228425f0f053d8af8df135c2fc570bc3d7
Adam Mak <maka9@mcmaster.ca->
2021-11-23 14:52:06 -0500

    Third draft of PartA

--- Ass3/bonus.m
+++ Ass3/bonus.m
@@ -10,11 +10,33 @@ overlapBFV = 5;
 overlapEEG = 28;
 
 nfftBFV = 1028;
-nfftEEG = 
+nfftEEG = 5e3;
 
-figure('Name', 'Spectrogram of BFVdu', 'NumberTitle', 'off');
-spectrogram(BFVdu, 'yaxis')
-figure('Name', 'Spectrogram of EEG1', 'NumberTitle', 'off');
-spectrogram(EEG1)
-figure('Name', 'Spectrogram of EEG2', 'NumberTitle', 'off');
-spectrogram(EEG2)
+figure('Name', 'Spectrogram', 'NumberTitle', 'off');
+
+subplot(3,1,1);
+[s_BFV, f_BFV, t_BFV] = spectrogram(BFVdu, winlenBFV, overlapBFV, nfftBFV, FsBFV);
+imagesc(t_BFV, f_BFV, abs(s_BFV)/winlenBFV);
+axis xy;
+ylim([0 10]);
+title('BFVdu');
+cb1 = colorbar;
+cb1.Label.String = '|X(f)| (mV)';
+
+subplot(3,1,2);
+[s_EEG1, f_EEG1, t_EEG1] = spectrogram(EEG1, winlenEEG, overlapEEG, nfftEEG, FsEEG);
+imagesc(t_EEG1, f_EEG1, abs(s_EEG1)/winlenEEG)
+axis xy;
+ylim([0 10]);
+title('EEG1');
+cb2 = colorbar;
+cb2.Label.String = '|X(f)| (mV)';
+
+subplot(3,1,3);
+[s_EEG2, f_EEG2, t_EEG2] = spectrogram(EEG2, winlenEEG, overlapEEG, nfftEEG, FsEEG);
+imagesc(t_EEG2, f_EEG2, abs(s_EEG2)/winlenEEG)
+axis xy;
+ylim([0 10]);
+title('EEG2');
+cb3 = colorbar;
+cb3.Label.String = '|X(f)| (mV)';
--- Ass3/partA.m
+++ Ass3/partA.m
@@ -1,17 +1,67 @@
 load('BFVdata_assignment3.mat');
 
-L = 300;
+t = 0:1/Fs:(length(BFVdu)-1)/Fs;
+[xpks, xlocs] = findpeaks(BFVdu, 'MinPeakHeight', 30);
 
-[mBFV1, phBFV1, fBFV1] = fourier_dt(BFVdu, Fs, 'full');
-[mBFV2, phBFV2, fBFV2] = fourier_dt(BFVdu(1:L), Fs, 'full');
-[mBFV3, phBFV3, fBFV3] = fourier_dt([BFVdu(1:L)' zeros(1,L)]', Fs, 'full');
+% plot input signal
 
-figure('Name', 'Magnitude Spectrum of Entire BFVdu Signal', 'NumberTitle', 'off');
-plot(fBFV1, mBFV1)
-xlim([-30 30]);
-figure('Name', 'Magnitude Spectrum of Portion of BFVdu Signal', 'NumberTitle', 'off');
-plot(fBFV2, mBFV2)
-xlim([-30 30]);
-figure('Name', 'Magnitude Spectrum of Portion with Zero-pad', 'NumberTitle', 'off');
-plot(fBFV3, mBFV3)
-xlim([-30 30]);
+figure('Name', 'BFVdu Signal', 'NumberTitle', 'off');
+plot(t, BFVdu)
+hold on;
+plot(t(xlocs), xpks, 'r+')
+ylabel('Amplitude (mV)');
+title('BFVdu Signal');
+
+L = 226; % appx. length of period
+
+BFVdu_p = BFVdu(1:L);
+BFVdu_p_zp = [BFVdu_p' zeros(1,L)]';
+
+[mBFV, phBFV, fBFV] = fourier_dt(BFVdu, Fs, 'half');
+[mBFV_p, phBFV_p, fBFV_p] = fourier_dt(BFVdu_p, Fs, 'half');
+[mBFV_p_zp, phBFV_p_zp, fBFV_p_zp] = fourier_dt(BFVdu_p_zp, Fs, 'half');
+
+% plot magnitude spectra
+
+figure('Name', 'Magnitude Spectrum', 'NumberTitle', 'off');
+
+subplot(3,1,1);
+plot(fBFV, mBFV)
+xlim([0 30]);
+ylabel('|X(f)| (mV)');
+title('Entire BFVdu Signal');
+
+subplot(3,1,2);
+plot(fBFV_p, mBFV_p)
+xlim([0 30]);
+ylabel('|X(f)| (mV)');
+title('Portion of BFVdu Signal');
+
+subplot(3,1,3);
+plot(fBFV_p_zp, mBFV_p_zp)
+xlim([0 30]);
+ylabel('|X(f)| (mV)');
+title('Portion of BFVdu Signal with Zero-padding');
+
+% compare magnitude spectra
+
+figure('Name', 'Magnitude Spectra Comparison', 'NumberTitle', 'off');
+
+subplot(2,1,1);
+plot(fBFV, mBFV)
+hold on;
+plot(fBFV_p, mBFV_p, '+-')
+xlim([0 30]);
+ylabel('|X(f)| (mV)');
+title('BFVdu');
+legend('Full Signal', 'Portioned Signal');
+
+subplot(2,1,2);
+plot(fBFV, mBFV)
+hold on;
+plot(fBFV_p_zp, mBFV_p_zp, '-')
+xlim([0 30]);
+ylabel('|X(f)| (mV)');
+title('BFVdu');
+legend('Full Signal', 'Zero-padded Portioned Signal');
+xlabel('f (Hz)');
