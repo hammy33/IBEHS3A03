6080d10872c181139998dc2dec0f55cb5a6e2d5f
Adam Mak <maka9@mcmaster.ca->
2021-11-22 23:14:43 -0500

    Second Draft of PartB

--- Ass3/partA.m
+++ Ass3/partA.m
@@ -2,16 +2,16 @@ load('BFVdata_assignment3.mat');
 
 L = 300;
 
-[mBFV1, phBFV1, fBFV1] = fourier_dt(BFVdu, Fs, 'half');
-[mBFV2, phBFV2, fBFV2] = fourier_dt(BFVdu(1:L), Fs, 'half');
-[mBFV3, phBFV3, fBFV3] = fourier_dt([BFVdu(1:L)' zeros(1,L)]', Fs, 'half');
+[mBFV1, phBFV1, fBFV1] = fourier_dt(BFVdu, Fs, 'full');
+[mBFV2, phBFV2, fBFV2] = fourier_dt(BFVdu(1:L), Fs, 'full');
+[mBFV3, phBFV3, fBFV3] = fourier_dt([BFVdu(1:L)' zeros(1,L)]', Fs, 'full');
 
 figure('Name', 'Magnitude Spectrum of Entire BFVdu Signal', 'NumberTitle', 'off');
 plot(fBFV1, mBFV1)
-xlim([0 30]);
+xlim([-30 30]);
 figure('Name', 'Magnitude Spectrum of Portion of BFVdu Signal', 'NumberTitle', 'off');
 plot(fBFV2, mBFV2)
-xlim([0 30]);
+xlim([-30 30]);
 figure('Name', 'Magnitude Spectrum of Portion with Zero-pad', 'NumberTitle', 'off');
 plot(fBFV3, mBFV3)
-xlim([0 30]);
+xlim([-30 30]);
