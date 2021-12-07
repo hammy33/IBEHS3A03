7e7d470f6facd66cf29cc1e50f145248090e8485
Hamrish Saravanakumar <saravah@mcmaster.ca>
2021-12-07 13:01:16 -0500

    initial implementation

---
+++ Ass4/EEG_FIR.m
@@ -0,0 +1,28 @@
+load('EEGdata_assignment4.mat');
+
+%Input Time-Domain Waveform Plot
+n = 0:(length(EEGa4)-1);
+t = n/Fs;
+figure
+plot(t,EEGa4)
+
+%Magnitude and Phase spectra of the Input Waveform
+[M,ph,f] = fourier_dt(EEGa4,Fs,'half');
+figure
+plot(f,M)
+
+%Output Time-Domain with Filter against Input Time-Domain
+y = filter(Hd_EEG_FIR,EEGa4);
+figure
+plot(t,y)
+hold on
+plot(t,EEGa4)
+shg
+
+%Magnitude and Phase spectra of Output Waveform
+[My,phy,fy] = fourier_dt(y,Fs,'half');
+figure
+plot(fy,My)
+hold on
+plot(f,M)
+shg
